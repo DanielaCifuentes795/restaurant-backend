e6a03b9fa52582a50a3f4040523eb32a57e8c974
Daniela Cifuentes <dcifuentes109623@umanizales.edu.co>
2026-09-06 15:50:11 -0500

    Add Express server setup

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-node_modules/
-.env
+const express = require('express')
 
+const app = express()
+const PORT = 3000
 
+app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.send('Server is running successfully')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`)
+})
