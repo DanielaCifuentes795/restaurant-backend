4c6ba0ac484ee7e667b6a3412f376f9466bbd047
Daniela Cifuentes <dcifuentes109623@umanizales.edu.co>
2026-09-07 20:48:41 -0500

    Configuración de modelos según clases en DB

---
+++ src/models/index.js
@@ -0,0 +1,53 @@
+const sequelize = require('../config/database')
+const User = require('./user.model')
+const Product = require('./product.model')
+const Sale = require('./sale.model')
+const SaleProduct = require('./saleProduct.model')
+
+User.hasMany(Sale, {
+    foreignKey: 'userId',
+    as: 'sales',
+    onDelete: 'RESTRICT',
+    onUpdate: 'CASCADE'
+})
+
+Sale.belongsTo(User, {
+    foreignKey: 'userId',
+    as: 'user'
+})
+
+Sale.hasMany(SaleProduct, {
+    foreignKey: 'saleId',
+    as: 'saleProducts',
+    onDelete: 'RESTRICT',
+    onUpdate: 'CASCADE'
+})
+
+SaleProduct.belongsTo(Sale, {
+    foreignKey: 'saleId',
+    as: 'sale'
+})
+
+Product.hasMany(SaleProduct, {
+    foreignKey: 'productId',
+    as: 'productsSales',
+    onDelete: 'RESTRICT',
+    onUpdate: 'CASCADE'
+})
+
+SaleProduct.belongsTo(Product, {
+    foreignKey: 'productId',
+    as: 'product'
+})
+
+
+module.exports = {
+    sequelize,
+    User,   
+    Product,
+    Sale,
+    SaleProduct
+}
+
+
+
